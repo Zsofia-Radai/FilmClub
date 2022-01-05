f09832476d84931d08eff5e1cd7c2c218b494eae
unknown <zsofiaradai@gmail.com>
2022-01-05 17:21:15 +0100

    make navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import { UserAddIcon } from "@heroicons/react/solid";
-import { LoginIcon, MenuIcon} from "@heroicons/react/outline";
+import { LoginIcon, MenuIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 
 function Navbar() {
@@ -24,32 +24,40 @@ function Navbar() {
 
 	return (
 		<nav>
-			<MenuIcon className="menu-icon" onClick={toggleNav} />
+			<MenuIcon className="hamburger-icon" onClick={toggleNav} />
+
+			<div className="logo">FilmClub</div>
 
 			{(toggleMenu || screenWidth > 1200) && (
-				<ul className="menu">
-					<li className="item">Filmclub</li>
-					<li className="item has-submenu">
-						Categories
-						{/* <ul className="submenu">
-						<li>Action</li>
-						<li>Adventure</li>
-						<li>Drama</li>
-						<li>Horror</li>
-					</ul> */}
-					</li>
-					<li className="item">
-						<input className="search" type="text" placeholder="Search in titles" />
-					</li>
-					<li className="item user-admission">
-						<UserAddIcon className="icon" />
-						<div>Sign up</div>
-					</li>
-					<li className="item user-admission">
-						<LoginIcon className="icon" />
-						<div>Log In</div>
-					</li>
-				</ul>
+				<div className="menu">
+					<ul>
+						<li className="item has-submenu">
+							Categories
+							{/* <ul className="submenu">
+								<li>Action</li>
+								<li>Adventure</li>
+								<li>Drama</li>
+								<li>Horror</li>
+							</ul> */}
+						</li>
+						<li className="item">
+							<input
+								className="search"
+								type="text"
+								placeholder="Search in titles"
+							/>
+						</li>
+						
+						<li className="item user-admission">
+							<UserAddIcon className="icon" />
+							<div>Sign up</div>
+						</li>
+						<li className="item user-admission">
+							<LoginIcon className="icon" />
+							<div>Log In</div>
+						</li>
+					</ul>
+				</div>
 			)}
 		</nav>
 	);
